2dc32bb7c3d40002b086f384821189a0f41f5e0e
rohi <rohialuede@gmail.com>
2025-02-18 00:16:20 +0000

    resolved merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 export default {
     theme: {
@@ -31,4 +32,39 @@ export default {
 
 <script>
   AOS.init();
+=======
+
+export default {
+    theme: {
+      extend: {
+
+        
+        animation: {
+          'fade-in': 'fadeIn 0.3s ease both',
+        },
+        keyframes: {
+          fadeIn: {
+            from: { opacity: 0 },
+            to: { opacity: 1 },
+          },
+        },
+
+        fontFamily: {
+          productsans: ['productsans', 'sans-serif'],
+        }
+      },
+    },
+
+
+
+
+
+
+  };
+
+  
+
+<script>
+  AOS.init();
+>>>>>>> 877ff19 (Second commit)
 </script>
